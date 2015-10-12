3c101d394e4322869003b93211f35a34304576f4
Thomas Schubert <funthomas424242@gmail.com>
2015-10-13 00:06:11 +0200

    port expose 8080

--- src/main/scripts/build.sh
+++ src/main/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker rm -f "spring-sts-ide.docker"
 docker build -t "funthomas424242/spring-sts-ide.docker" ../docker/
-docker run --name "spring-sts-ide.docker" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/spring-sts-ide.docker:latest
+docker run -p 8080:8080 --name "spring-sts-ide.docker" -it -e DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix funthomas424242/spring-sts-ide.docker:latest
 
 
